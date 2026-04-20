7a55c76f4a0a0840fb7c057b8cd87ea8eada2eba
sc9593 <sc9593095@gmail.com>
2026-04-20 21:09:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,20 @@
+import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 import json
 from telegram import (
     Update,
